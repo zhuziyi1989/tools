b76364c8b9177e7f702398d3bb95eb2c5d3cc97f
zhuziyi <zhuziyi@jd.com>
2016-08-07 21:44:38 +0800

    更新查询频繁导致接口拒绝等问题

--- wechat-deleted-friends/wdf.py
+++ wechat-deleted-friends/wdf.py
@@ -438,7 +438,7 @@ def heartBeatLoop():
 
 def main():
     global myRequests
-	
+    
     if hasattr(ssl, '_create_unverified_context'):
         ssl._create_default_https_context = ssl._create_unverified_context
 
