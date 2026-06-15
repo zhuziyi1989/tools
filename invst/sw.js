ec4a54e9bf5a53e509365382167ae91311ad83cf
zhuziyi/Venus <im.zhuziyi@gmail.com>
2026-06-16 02:40:02 +0800

    feat: 添加 PWA 支持，包含 manifest.json 和 Service Worker

---
+++ invst/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'asset-tracker-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png'
+];
+
+// 安装并缓存资源
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 拦截网络请求，优先使用本地缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
